5518da935d8272ee9a2b780e1535601967dd1783
dhirajgolhar <32592879+dhirajgolhar@users.noreply.github.com>
2019-07-31 15:25:11 +0530

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -4,6 +4,6 @@ class ddd
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, meerut"); 
+        System.out.println("Hello, multiwin"); 
     } 
 } 
